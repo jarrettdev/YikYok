70b9e5ca3f0346959aed6c67988fcca6bd2f1ae1
Jarrett <moonwet@protonmail.com>
2020-05-08 14:23:41 -0500

    user agents

--- Request.py
+++ Request.py
@@ -1,1 +1,7 @@
 import requests
+
+base_url = 'https://www.tiktok.com/trending?lang=en'
+
+req = requests.get(base_url, headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"})
+
+print('request', req.text)
